3318a98c72f046c1e68d7baedd18d9bd50c67f28
S1ngS1ng <liuxing0514@gmail.com>
2017-02-13 23:58:18 -0600

    feat(config) Update webpack config, remove unused directory

--- webpack.config.js
+++ webpack.config.js
@@ -205,13 +205,7 @@ module.exports = function makeWebpackConfig() {
 
             // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
             // Minify all javascript, switch loaders to minimizing mode
-            new webpack.optimize.UglifyJsPlugin(),
-
-            // Copy assets from the public folder
-            // Reference: https://github.com/kevlened/copy-webpack-plugin
-            new CopyWebpackPlugin([{
-                from: __dirname + '/src/public'
-            }])
+            new webpack.optimize.UglifyJsPlugin()
         )
     }
 
